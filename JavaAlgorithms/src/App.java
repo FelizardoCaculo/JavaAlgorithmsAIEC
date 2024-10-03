1aef44e885c17f316b8724f61c08277cbcd20032
FelizardoCaculo <felizardo1987@gmail.com>
2024-10-03 04:31:54 +0100

    fixing project file structure

--- JavaAlgorithms/src/App.java
+++
@@ -1,6 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("\nHello, World!");
-        System.out.println("\nSeja bem vido ao nosso curso de Programação em Java");
-    }
-}
